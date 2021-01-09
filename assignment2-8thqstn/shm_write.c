8e253f014c53ee696e435d0ce21dd969d6b1577c
99003170 <75420372+99003170@users.noreply.github.com>
2021-01-09 15:45:51 +0530

    Update shm_write.c

--- assignment2-8thqstn/shm_write.c
+++ assignment2-8thqstn/shm_write.c
@@ -3,18 +3,18 @@
 
 int main()
 {
-	int filesz=4096; 
-	int fd,offset=0;
+	int file_size=4096; 
+	int file1,offset=0;
 	sem_t *ps,*qs;
 	ps=sem_open("/s1",O_CREAT, 0777, 1);
 	qs=sem_open("/s2",O_CREAT, 0777, 0);
-	fd=shm_open("/shm1",O_CREAT|O_RDWR,0666);
-	if(fd<0)
+	file1=shm_open("/shm1",O_CREAT|O_RDWR,0666);
+	if(file1<0)
 	{
-		perror("open");
+		printf("failed to open");
 		exit(1);
 	}
-    ftruncate(fd, filesz);
+    ftruncate(file1, file_size);
 	void *pbase;
 	pbase=mmap(0,filesz, PROT_WRITE, MAP_SHARED,fd,offset);
 	if(pbase==MAP_FAILED)
@@ -22,13 +22,13 @@ int main()
 		fprintf(stderr,"mapping failed\n");
 		exit(1);
 	}
-	char buf[64];
+	char buffer[64];
     char str[]="SHASHANKVODELALTTSMANYATATECHPARK";
        sem_wait(ps);
 	strncpy(pbase,str,20);	
 	sem_post(ps);
 	sem_post(qs);
-	printf("buf=%s\n",str);
+	printf("buffer=%s\n",str);
 	munmap(pbase,filesz);
 	return 0;
 }
